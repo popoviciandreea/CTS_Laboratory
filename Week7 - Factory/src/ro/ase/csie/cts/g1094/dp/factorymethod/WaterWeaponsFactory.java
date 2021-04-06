027bd68b6703e87de1c08ab7b7933aa9b6721f13
Popovici <andr.popovici98@yahoo.com>
2021-04-06 15:26:05 +0300

    Factory method implementation

---
+++ Week7 - Factory/src/ro/ase/csie/cts/g1094/dp/factorymethod/WaterWeaponsFactory.java
@@ -0,0 +1,30 @@
+package ro.ase.csie.cts.g1094.dp.factorymethod;
+
+import ro.ase.csie.cts.g1094.dp.simplefactory.AbstractWeapon;
+import ro.ase.csie.cts.g1094.dp.simplefactory.WeaponType;
+
+public class WaterWeaponsFactory extends AbstractFactory{
+
+	@Override
+	public AbstractWeapon getWeapon(WeaponType type, String description) {
+		
+		AbstractWeapon weapon = null;
+		
+		switch (type) {
+		case PISTOL:
+			weapon = new WaterPistol();
+			break;
+		case MACHINE_GUN:
+			weapon = new WaterBallon();
+			break;
+		case BAZOOKA:
+			weapon = new WaterBucket();
+			break;
+			default:
+				throw new UnsupportedOperationException ("Type not covered");
+		}
+		
+		return weapon;
+	}
+
+}
